e8896f09041c2a18a9f16c5f6d5e28453f72838f
KheangLov <39585280+KheangLov@users.noreply.github.com>
2019-12-08 20:02:24 +0700

    Get readme

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,89 +9,89 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware(['auth','verified']);
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware(['auth', 'verified']);
+	}
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:50', 'unique:users'],
-            'email' => ['required', 'string', 'email', 'max:50', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role_id' => ['required'],
-        ]);
-    }
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'name' => ['required', 'string', 'max:50', 'unique:users'],
+			'email' => ['required', 'string', 'email', 'max:50', 'unique:users'],
+			'password' => ['required', 'string', 'min:8', 'confirmed'],
+			'role_id' => ['required'],
+		]);
+	}
 
-    public function index()
-    {
-        $users = User::join('roles', 'users.role_id', '=', 'roles.id')
-            ->select('users.id', 'users.name', 'users.email', 'roles.name AS role_name')
-            ->orderBy('roles.name')
-            ->get();
-        return view('list', ["users" => $users]);
-    }
+	public function index()
+	{
+		$users = User::join('roles', 'users.role_id', '=', 'roles.id')
+			->select('users.id', 'users.name', 'users.email', 'roles.name AS role_name')
+			->orderBy('roles.name')
+			->get();
+		return view('list', ["users" => $users]);
+	}
 
-    public function destroy($id)
-    {
-        $user = User::find($id);
-        $user->delete();
-        return redirect()
-            ->route('user_list')
-            ->with('success','User deleted successfully');
-    }
+	public function destroy($id)
+	{
+		$user = User::find($id);
+		$user->delete();
+		return redirect()
+			->route('user_list')
+			->with('success', 'User deleted successfully');
+	}
 
-    public function add()
-    {
-        $roles = Role::all()->sortBy("name");
-        return view('add', compact("roles"));
-    }
+	public function add()
+	{
+		$roles = Role::all()->sortBy("name");
+		return view('add', compact("roles"));
+	}
 
-    public function create(Request $request)
-    {
-        $user = new User;
-        $roles = Role::all()->sortBy("name");
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->role_id = $request->role_id;
-        $user->save();
-        return redirect()
-            ->route('user_add', compact("roles"))
-            ->with('success','User created successfully');
-    }
+	public function create(Request $request)
+	{
+		$user = new User;
+		$roles = Role::all()->sortBy("name");
+		$user->name = $request->name;
+		$user->email = $request->email;
+		$user->password = Hash::make($request->password);
+		$user->role_id = $request->role_id;
+		$user->save();
+		return redirect()
+			->route('user_add', compact("roles"))
+			->with('success', 'User created successfully');
+	}
 
-    public function edit($id)
-    {
-        $user = User::find($id);
-        $roles = Role::all()->sortBy("name");
-        return view('edit', ['user' => $user, 'roles' => $roles]);
-    }
+	public function edit($id)
+	{
+		$user = User::find($id);
+		$roles = Role::all()->sortBy("name");
+		return view('edit', ['user' => $user, 'roles' => $roles]);
+	}
 
-    public function update(Request $request, $id)
-    {
-        $user = User::find($id);
-        $roles = Role::all()->sortBy("name");
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->role_id = $request->role_id;
-        $user->save();
-        $user->update();
-        return redirect()
-            ->route('user_edit', ['id' => $id, 'roles' => $roles])
-            ->with('success','User edited successfully');
-    }
+	public function update(Request $request, $id)
+	{
+		$user = User::find($id);
+		$roles = Role::all()->sortBy("name");
+		$user->name = $request->name;
+		$user->email = $request->email;
+		$user->password = Hash::make($request->password);
+		$user->role_id = $request->role_id;
+		$user->save();
+		$user->update();
+		return redirect()
+			->route('user_edit', ['id' => $id, 'roles' => $roles])
+			->with('success', 'User edited successfully');
+	}
 }
